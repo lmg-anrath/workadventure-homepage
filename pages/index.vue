c8f313e0dbe1b678ead7b4f50295ab35aade249e
m1kx <mika.rottlaender@icloud.com>
2023-09-19 22:42:28 +0200

    Initial Upload

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import '../assets/main.sass';
+import '../assets/mobile.sass';
+</script>
+
+<template>
+  <div>
+    <div class="main-container">
+      <div class="heading">
+        WORKADVENTURE<div>LMG</div>
+      </div>
+      <div class="play-button">
+        <button>JETZT ÖFFNEN!</button>
+        <div class="hover"></div>
+      </div>
+    </div>
+    <div class="background">
+      <img src="https://www.lmg-anrath.de/wp-content/uploads/2017/10/LM-Logo_blau.png" alt="">
+    </div>
+  </div>
+</template>
